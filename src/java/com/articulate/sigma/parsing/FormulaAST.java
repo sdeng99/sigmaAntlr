53e58b2c5f6d485e73f38add61056a64840ae590
apease <apease@articulatesoftware.com>
2020-07-23 20:58:17 -0700

    first working version

--- src/java/com/articulate/sigma/parsing/FormulaAST.java
+++ src/java/com/articulate/sigma/parsing/FormulaAST.java
@@ -2,6 +2,7 @@ package com.articulate.sigma.parsing;
 
 import com.articulate.sigma.Formula;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
 
@@ -10,6 +11,65 @@ public class FormulaAST extends Formula {
     // arguments to relations in order to find the types of arg in a second pass
     // first key is a relation name, interior key is argument number starting at 1
     public HashMap<String, HashMap<Integer, HashSet<SuokifParser.ArgumentContext>>> argMap = new HashMap<>();
+    public HashMap<String,HashSet<String>> varmap = new HashMap<>();
+
+    /** ***************************************************************
+     * Merge arguments to a predicate, which may themselves be complex
+     * formulas, with an existing formula.
+     */
+    public FormulaAST mergeFormulaAST(FormulaAST f2) {
+
+        this.allVarsCache.addAll(f2.allVarsCache);
+        this.allVarsPairCache.addAll(f2.allVarsPairCache);
+        this.quantVarsCache.addAll(f2.quantVarsCache);
+        this.unquantVarsCache.addAll(f2.unquantVarsCache);
+        this.existVarsCache.addAll(f2.existVarsCache);
+        this.univVarsCache.addAll(f2.univVarsCache);
+        this.termCache.addAll(f2.termCache);
+        if (this.rowVarCache == null)
+            this.rowVarCache = new HashSet<>();
+        if (f2.rowVarCache == null)
+            f2.rowVarCache = new HashSet<>();
+        this.rowVarCache.addAll(f2.rowVarCache);
+        if (this.predVarCache == null)
+            this.predVarCache = new HashSet<>();
+        if (f2.predVarCache == null)
+            f2.predVarCache = new HashSet<>();
+        this.predVarCache.addAll(f2.predVarCache);
+        this.argMap.putAll(f2.argMap);
+        this.varmap.putAll(f2.varmap);
+        return this;
+    }
+
+    /** ***************************************************************
+     * Merge arguments to a predicate, which may themselves be complex
+     * formulas, with an existing formula.
+     */
+    public FormulaAST mergeFormulaAST(ArrayList<FormulaAST> ar) {
+
+        if (this.predVarCache == null)
+            this.predVarCache = new HashSet<>();
+        if (this.rowVarCache == null)
+            this.rowVarCache = new HashSet<>();
+        for (FormulaAST arf : ar) {
+            this.allVarsCache.addAll(arf.allVarsCache);
+            this.allVarsPairCache.addAll(arf.allVarsPairCache);
+            this.quantVarsCache.addAll(arf.quantVarsCache);
+            this.unquantVarsCache.addAll(arf.unquantVarsCache);
+            this.existVarsCache.addAll(arf.existVarsCache);
+            this.univVarsCache.addAll(arf.univVarsCache);
+            this.termCache.addAll(arf.termCache);
+            if (arf.rowVarCache == null)
+                arf.rowVarCache = new HashSet<>();
+            this.rowVarCache.addAll(arf.rowVarCache);
+            if (arf.predVarCache == null)
+                arf.predVarCache = new HashSet<>();
+            this.predVarCache.addAll(arf.predVarCache);
+            this.argMap.putAll(arf.argMap);
+            this.varmap.putAll(arf.varmap);
+        }
+        return this;
+    }
 
     /** *****************************************************************
      * the textual version of the formula
@@ -20,5 +80,26 @@ public class FormulaAST extends Formula {
         f.setFormula(input);
         f.comment = true;
         return f;
+    }
+
+    /** *****************************************************************
+     * the textual version of the formula
+     */
+    public void printCaches() {
+
+        super.printCaches();
+        System.out.println("argMap: ");
+        for (String pred : argMap.keySet()) {
+            System.out.print("\t" + pred + "\t");
+            for (Integer i : argMap.get(pred).keySet()) {
+                System.out.print(i + ": ");
+                for (SuokifParser.ArgumentContext c : argMap.get(pred).get(i)) {
+                    System.out.print(c.getText() + ", ");
+                }
+            }
+            System.out.println();
+        }
+        System.out.println("varmap: " + varmap);
+        System.out.println();
     }
 }
