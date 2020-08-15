1ced44544471ad2b658a0ba9aab9e415b39104c2
apease <apease@articulatesoftware.com>
2020-08-14 17:47:46 -0700

    test method to invoke all tests

---
+++ src/test/java/com/articulate/sigma/parsing/AllTests.java
@@ -0,0 +1,12 @@
+package com.articulate.sigma.parsing;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+        IntegrationTests.class,
+        UnitTests.class,
+})
+public class AllTests {
+}
