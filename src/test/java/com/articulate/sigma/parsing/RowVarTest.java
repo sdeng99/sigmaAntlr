0eca2cb2f145cf23c6053046e9d4b71599ee8c67
apease <apease@articulatesoftware.com>
2020-08-14 17:56:01 -0700

    update tests to conform to the current RowVar clss

--- src/test/java/com/articulate/sigma/parsing/RowVarTest.java
+++ src/test/java/com/articulate/sigma/parsing/RowVarTest.java
@@ -10,6 +10,7 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 
 import java.util.HashMap;
+import java.util.HashSet;
 
 import static org.junit.Assert.assertEquals;
 
@@ -30,9 +31,9 @@ public class RowVarTest {
 
     /***************************************************************
      * */
-    public static HashMap<Integer,FormulaAST> process(String input) {
+    public static HashSet<FormulaAST> process(String input) {
 
-        System.out.println(input);
+        System.out.println("RowVarTest.process(): " + input);
         CodePointCharStream inputStream = CharStreams.fromString(input);
         SuokifLexer suokifLexer = new SuokifLexer(inputStream);
         CommonTokenStream commonTokenStream = new CommonTokenStream(suokifLexer);
@@ -40,21 +41,26 @@ public class RowVarTest {
         SuokifParser.FileContext fileContext = suokifParser.file();
         SuokifVisitor visitor = new SuokifVisitor();
         HashMap<Integer,FormulaAST> hm = visitor.visitFile(fileContext);
+        if (visitor.hasPredVar.size() > 0) {
+            System.out.println("Error - can't have tests with pred vars in this routine.");
+            return null;
+        }
         PredVarInst pvi = new PredVarInst(kb);
         Sortals sortals = new Sortals(kb);
         VarTypes vt = new VarTypes(hm.values(),kb);
         vt.findTypes();
-        RowVar rv = new RowVar(kb);
         for (FormulaAST f : visitor.rules) {
-            System.out.println("RowVarTest.process(): before winnow");
-            f.printCaches();
+            //System.out.println("RowVarTest.process(): before winnow");
+            //f.printCaches();
             sortals.winnowAllTypes(f);
-            System.out.println("RowVarTest.process(): after winnow");
-            f.printCaches();
+            //System.out.println("RowVarTest.process(): after winnow");
+            //f.printCaches();
         }
-        pvi.processAll(visitor.hasPredVar);
-        rv.expandRowVar(visitor.hasRowVar);
-        return hm;
+        PredVarInst.predVarInstDone = true; // because the test formulas already did it
+        RowVar rv = new RowVar(kb);
+        HashSet<FormulaAST> rvs = rv.expandRowVar(visitor.hasRowVar);
+        //System.out.println("RowVarTest.process(): rvs" + rvs);
+        return rvs;
     }
 
     /** ***************************************************************
@@ -62,16 +68,20 @@ public class RowVarTest {
     @Test
     public void test1() {
 
-        String input = "(likes John Mary)\n; and here's a comment\n(part Wheel1 Car2)\n";
+        String input = "\n" +
+                "(=>\n" +
+                "    (and\n" +
+                "        (contraryAttribute @ROW1)\n" +
+                "        (identicalListItems\n" +
+                "            (ListFn @ROW1)\n" +
+                "            (ListFn @ROW2)))\n" +
+                "    (contraryAttribute @ROW2))";
 
-        HashMap<Integer,FormulaAST> hm = process(input);
-        StringBuilder sb = new StringBuilder();
-        for (Formula f : hm.values()) {
-            f.printCaches();
-            sb.append(f.getFormula() + "\n");
-        }
-        System.out.println("result: " + sb);
-        assertEquals(input,sb.toString());
+        HashSet<FormulaAST> hm = process(input);
+        System.out.println("one result: " + hm.iterator().next());
+        System.out.println("RowVarTest.test1(): result size: " + hm.size());
+        System.out.println("RowVarTest.test1(): expected size: " + 49);
+        assertEquals(49,hm.size());
         System.out.println();
     }
 
@@ -82,11 +92,11 @@ public class RowVarTest {
 
         String input = "(=> (and (minValue part ?ARG ?N) (part @ARGS) (equal ?VAL (ListOrderFn (ListFn @ARGS) ?ARG))) (greaterThan ?VAL ?N))";
         String expected = "(=> (and (minValue part ?ARG ?N) (part ?ARGS1 ?ARGS2) (equal ?VAL (ListOrderFn (ListFn ?ARGS1 ?ARGS2) ?ARG))) (greaterThan ?VAL ?N))";
-        HashMap<Integer,FormulaAST> hm = process(input);
+        HashSet<FormulaAST> hm = process(input);
         StringBuilder sb = new StringBuilder();
-        for (Formula f : hm.values()) {
+        for (FormulaAST f : hm) {
             f.printCaches();
-            sb.append(f.getFormula() + " ");
+            sb.append(f.getFormula() + "\n");
         }
         System.out.println("result: " + sb);
         assertEquals(expected,sb.toString().trim());
