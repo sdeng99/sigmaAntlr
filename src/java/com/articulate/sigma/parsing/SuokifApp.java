e8c6776e029e36da7a8ae39047f35818fc3a20de
git <tdnorbra@nps.edu>
2025-03-08 09:23:14 -0800

    [Terry N.] add parse listener

--- src/java/com/articulate/sigma/parsing/SuokifApp.java
+++ src/java/com/articulate/sigma/parsing/SuokifApp.java
@@ -4,11 +4,13 @@ import org.antlr.v4.runtime.*;
 
 public class SuokifApp {
 
-    public static void process(CodePointCharStream inputStream) {
+    public static void process(CharStream inputStream) {
 
         SuokifLexer suokifLexer = new SuokifLexer(inputStream);
         CommonTokenStream commonTokenStream = new CommonTokenStream(suokifLexer);
         SuokifParser suokifParser = new SuokifParser(commonTokenStream);
+        suokifParser.removeErrorListeners();
+        suokifParser.addErrorListener(new SuokifParserErrorListener());
         SuokifParser.FileContext fileContext = suokifParser.file();
         SuokifVisitor visitor = new SuokifVisitor();
         visitor.visitFile(fileContext);
@@ -18,7 +20,7 @@ public class SuokifApp {
 
         String input = "(likes John Mary) ; and here's a comment\n";
         System.out.println(input);
-        CodePointCharStream inputStream = CharStreams.fromString(input);
+        CharStream inputStream = CharStreams.fromString(input);
         process(inputStream);
         System.out.println();
 
