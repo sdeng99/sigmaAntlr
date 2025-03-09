3102a09a7340688fd01f52fa1dfb34ecc2de9ca0
git <tdnorbra@nps.edu>
2025-03-08 23:29:27 -0800

    [Terry N.] expand examples

--- src/java/com/articulate/sigma/parsing/SuokifApp.java
+++ src/java/com/articulate/sigma/parsing/SuokifApp.java
@@ -1,33 +1,53 @@
 package com.articulate.sigma.parsing;
 
-import org.antlr.v4.runtime.*;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Map;
 
+/** Example App showing 3 various ways to parse SUO-KIF and show the results */
 public class SuokifApp {
 
-    public static void process(CharStream inputStream) {
-
-        SuokifLexer suokifLexer = new SuokifLexer(inputStream);
-        CommonTokenStream commonTokenStream = new CommonTokenStream(suokifLexer);
-        SuokifParser suokifParser = new SuokifParser(commonTokenStream);
-        suokifParser.removeErrorListeners();
-        suokifParser.addErrorListener(new SuokifParserErrorListener());
-        SuokifParser.FileContext fileContext = suokifParser.file();
-        SuokifVisitor visitor = new SuokifVisitor();
-        visitor.visitFile(fileContext);
+    public static void process(String input) {
+        SuokifVisitor.parseString(input);
     }
 
-    public static void main( String[] args) {
+    public static void process(FormulaAST fast) {
+        SuokifVisitor.parseFormula(fast);
+    }
 
-        String input = "(likes John Mary) ; and here's a comment\n";
-        System.out.println(input);
-        CharStream inputStream = CharStreams.fromString(input);
-        process(inputStream);
+    public static void process(File file) {
+        new SuokifVisitor().parseFile(file);
+    }
+
+    private static void showResults() {
+
+        Map<Integer, FormulaAST> hm = SuokifVisitor.result;
+        StringBuilder sb = new StringBuilder();
+        for (FormulaAST f : hm.values())
+            sb.append(f.getFormula()).append("\n");
+        System.out.println("result: " + sb);
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        System.out.println();
+        String input = "(likes John Mary)\n; and here's a comment\n";
+        process(input);
+        showResults();
         System.out.println();
 
         input = "(=> (and (minValue ?R ?ARG ?N) (?R @ARGS) (equal ?VAL (ListOrderFn (ListFn @ARGS) ?ARG))) (greaterThan ?VAL ?N))";
-        System.out.println(input);
-        inputStream = CharStreams.fromString(input);
-        process(inputStream);
+        FormulaAST fast = new FormulaAST();
+        fast.setFormula(input);
+        process(fast);
+        showResults();
         System.out.println();
+
+        File file = new File("testFormula.txt");
+        Path path = Paths.get(file.toURI());
+        process(path.toFile());
+        showResults();
     }
 }
