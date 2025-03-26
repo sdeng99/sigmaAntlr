e2d68d47ac2c87eaac15b8bc8bb1833a8374a7e3
git <tdnorbra@nps.edu>
2025-03-26 12:31:13 -0700

    [Terry N.] add showHelp

--- src/java/com/articulate/sigma/parsing/SuokifApp.java
+++ src/java/com/articulate/sigma/parsing/SuokifApp.java
@@ -47,6 +47,19 @@ public class SuokifApp {
         System.out.println("result: " + sb);
     }
 
+    /** ***************************************************************
+     */
+    public static void showHelp() {
+
+        System.out.println("SuokifApp class");
+        System.out.println("  options:");
+        System.out.println("  -h - show this help screen");
+        System.out.println("  -d - demo 3 various inputs to the parser");
+        System.out.println("       process(input)   - parse a string input");
+        System.out.println("       process(formula) - parse a formula");
+        System.out.println("       process(fname)   - parse a file");
+    }
+
     /** Command line entry point for the APP
      *
      * @param args common line arguments if any
@@ -54,22 +67,35 @@ public class SuokifApp {
      */
     public static void main(String[] args) throws IOException {
 
-        System.out.println();
-        String input = "(likes John Mary)\n; and here's a comment\n";
-        process(input);
-        showResults();
-        System.out.println();
+        System.out.println("INFO in SuokifApp.main()");
 
-        input = "(=> (and (minValue ?R ?ARG ?N) (?R @ARGS) (equal ?VAL (ListOrderFn (ListFn @ARGS) ?ARG))) (greaterThan ?VAL ?N))";
-        FormulaAST fast = new FormulaAST();
-        fast.setFormula(input);
-        process(fast);
-        showResults();
-        System.out.println();
+        if (args != null && args.length > 0 && args[0].equals("-h"))
+            showHelp();
+        else {
+            if (args != null && args.length == 1 && args[0].equals("-d")) {
+                System.out.println();
+                String input = "(likes John Mary)\n; and here's a comment\n";
+                process(input);
+                System.out.println("String input: " + input);
+                showResults();
+                System.out.println();
 
-        File file = new File("testFormula.txt");
-        Path path = Paths.get(file.toURI());
-        process(path.toFile());
-        showResults();
+                input = "(=> (and (minValue ?R ?ARG ?N) (?R @ARGS) (equal ?VAL (ListOrderFn (ListFn @ARGS) ?ARG))) (greaterThan ?VAL ?N))";
+                FormulaAST fast = new FormulaAST();
+                fast.setFormula(input);
+                process(fast);
+                System.out.println("Formula, or FormulaAST input\n: " + fast);
+                showResults();
+                System.out.println();
+
+                File file = new File("testFormula.txt");
+                Path path = Paths.get(file.toURI());
+                process(path.toFile());
+                System.out.println("File input: " + path);
+                showResults();
+            }
+            else
+                showHelp();
+        }
     }
 }
